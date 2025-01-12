4468706cde20b659cca3faef3836fa3e87d17d17
Aleksey Tikhonov <leohar@mail.ru>
2025-01-12 18:55:19 +0300

    make models

--- app/cafe/models.py
+++ app/cafe/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(unique=True)
+    price = models.DecimalField(default=0, max_digits=8, decimal_places=2)
+
+
+class Status(models.Model):
+    name = models.CharField(unique=True) 
+
+
+class Order(models.Model):
+    table_number = models.IntegerField()
+    items = models.ManyToManyField(Item, related_name='orders')
+    total_price = models.DecimalField(default=0, max_digits=8, decimal_places=2)
+    status = models.ForeignKey(Status, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    
