e0f3100c9d392dd66521bb2c283d13a2ecd77ad2
Aleksey Tikhonov <leohar@mail.ru>
2025-01-15 20:27:49 +0300

    Make order_list as table

--- app/cafe/urls.py
+++ app/cafe/urls.py
@@ -7,5 +7,8 @@ app_name = 'cafe'
 urlpatterns = [
     path('', views.index_page, name='index'),
     path('orders/', views.order_list, name='order-list'),
-    path('orders/create/', views.create_order, name='order-create'),
+    path('orders/create/', views.order_create, name='order-create'),
+    path('orders/find/', views.order_find, name='order-find'),
+    path('orders/<int:order_id>', views.order_detail, name='order-detail'),
+    
 ]
--- app/cafe/views.py
+++ app/cafe/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from .models import Order
+from .models import Order, OrderItem
 from .forms import OrderForm
 
 
@@ -7,7 +7,7 @@ def index_page(request):
     return render(request, 'cafe/index.html')
 
 
-def create_order(request):
+def order_create(request):
     if request.method == 'POST':
         form = OrderForm(request.POST)
         if form.is_valid():
@@ -16,9 +16,10 @@ def create_order(request):
     else:
         form = OrderForm()
     context = {
-        'form': form
+        'form': form,
+        'foods': OrderItem.objects.all(),
     }
-    return render(request, 'cafe/create_order.html', context=context)
+    return render(request, 'cafe/order_create.html', context=context)
 
 
 def order_list(request):
@@ -26,3 +27,11 @@ def order_list(request):
         'orders': Order.objects.all(),
     }
     return render(request, 'cafe/order_list.html', context=context)
+
+
+def order_detail(request):
+    pass
+
+
+def order_find(request):
+    return render(request, 'cafe/order_find.html')
